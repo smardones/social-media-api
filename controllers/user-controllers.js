dfb8b4a3526f2bfcbfeddfe0ac16fa7aadc04756
smardone <shaun.mardones@gmail.com>
2020-10-02 23:47:21 -0400

    finished user route methods

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -10,5 +10,31 @@ const userControllers = {
 
     getUserById({params}, res) {
         User.findOne({_id: params.id})
+            .then(dbUserInfo => res.json(dbUserInfo))
+            .catch(err => res.status(400).json({message: 'There is no user with that id'}));
+    },
+
+    createNewUser({body}, res) {
+        User.create(body)
+            .then(dbUserInfo => res.json(dbUserInfo))
+            .catch(err => res.status(400).json(err));
+    },
+
+    updateUser({params, body}, res) {
+        User.findOneAndUpdate({_id: params.id}, body, {new: true})
+            .then(dbUserInfo => res.json(dbUserInfo))
+            .catch(err => res.status(400).json({message: 'No user found with that id'}));
+    },
+
+    deleteUser({params}, res) {
+        User.findOneAndDelete({_id: params.id})
+            .then(dbUserInfo => {
+                if (!dbUserInfo) {
+                    res.status(404).json({message: 'No user found with that id'});
+                    return;
+                }
+                res.json(dbUserInfo)
+            })
+            .catch(err => res.status(400));
     }
 }

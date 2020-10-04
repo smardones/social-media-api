0eab1cf3db80aa63ccf32f65b2c4814362e12248
smardone <shaun.mardones@gmail.com>
2020-10-03 20:29:59 -0400

    created thoughts schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,43 @@
 const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const ReactionSchema = new Schema({
+
+})
 
 const ThoughtSchema = new Schema({
-    
-}
+    thoughtText: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(text) {
+                if (text.length() > 0 && text.length() <= 280) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+},
+{
+    toJSON: {
+        getters: true,
+        virtuals: true
+    }
+},
 )
+
+ThoughtSchema.virtuals('reactionCount').get(function() {
+    return this.reactions.length;
+});
